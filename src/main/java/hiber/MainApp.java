d9eceb75c062d02faa29ea1c2033015933d522cd
Shellder55 <thetwillfeed@gmail.com>
2023-08-20 19:48:49 +0600

    1

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,54 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+//      Car car1 = new Car("BMW", 5, new User("User5", "Lastname5", "user5@mail.ru"));
+//      Car car2 = new Car("BMW", 6, new User("User6", "Lastname6", "user6@mail.ru"));
+
+//      userService.add(new Car("BMW", 5, new User("User5", "Lastname5", "user5@mail.ru")));
+//      userService.add(new Car("BMW", 6, new User("User6", "Lastname6", "user6@mail.ru")));
+
+        Car car1 = new Car("BMW", 5);
+        Car car2 = new Car("BMW", 6);
+
+
+//        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+        userService.add(new User("User5", "Lastname5", "user5@mail.ru", car1));
+        userService.add(new User("User6", "Lastname6", "user6@mail.ru", car2));
+
+//      userService.add(new User("User5", "Lastname5", "user5@mail.ru", car1));
+//      userService.add(new User("User5", "Lastname5", "user5@mail.ru", car2));
+
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car Model = " + user.getModel());
+            System.out.println("Car Series = " + user.getSeries());
+            System.out.println();
+        }
+
+        context.close();
+    }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,59 @@
+package hiber.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model_car")
+    private String model;
+    @Column(name = "series_car")
+    private int series;
+
+    @OneToOne(mappedBy = "car", fetch = FetchType.LAZY)
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
